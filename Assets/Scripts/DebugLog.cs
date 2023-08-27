c43e54f55f4d9c8b0acc6a9f81a965424724016b
Chantal Burkhard <chante11@t-online.de>
2023-08-27 17:53:35 +0200

    updated launcher script and added documentation

--- Assets/Scripts/DebugLog.cs
+++ Assets/Scripts/DebugLog.cs
@@ -3,29 +3,48 @@ using UnityEngine;
 
 namespace Com.WIAP.Checkers
 {
-    /// taken from https://stackoverflow.com/questions/67704820/how-do-i-print-unitys-debug-log-to-the-screen-gui
-   
+    /// <summary>
+    /// Displays Unity's debug log messages on the screen using OnGUI.
+    /// Based on code from: https://stackoverflow.com/questions/67704820/how-do-i-print-unitys-debug-log-to-the-screen-gui
+    /// </summary>
     public class DebugLog : MonoBehaviour
     {
         uint qsize = 15;  // number of messages to keep
         Queue myLogQueue = new Queue();
 
-        void Start()
+        /// <summary>
+        /// Called when the script starts.
+        /// </summary>
+        private void Start()
         {
             Debug.Log("Started up logging.");
         }
 
-        void OnEnable()
+        /// <summary>
+        /// Called when the script is enabled.
+        /// Subscribes to the logMessageReceived event.
+        /// </summary>
+        private void OnEnable()
         {
             Application.logMessageReceived += HandleLog;
         }
 
-        void OnDisable()
+        /// <summary>
+        /// Called when the script is disabled.
+        /// Unsubscribes from the logMessageReceived event.
+        /// </summary>
+        private void OnDisable()
         {
             Application.logMessageReceived -= HandleLog;
         }
 
-        void HandleLog(string logString, string stackTrace, LogType type)
+        /// <summary>
+        /// Handles log messages and stores them in the queue.
+        /// </summary>
+        /// <param name="logString">The log message.</param>
+        /// <param name="stackTrace">The stack trace (if applicable).</param>
+        /// <param name="type">The type of log message (error, warning, etc.).</param>
+        private void HandleLog(string logString, string stackTrace, LogType type)
         {
             string formattedLog = "[" + type + "] : " + logString;
 
@@ -41,7 +60,10 @@ namespace Com.WIAP.Checkers
             }
         }
 
-        void OnGUI()
+        /// <summary>
+        /// Called to display the log messages on the screen using OnGUI.
+        /// </summary>
+        private void OnGUI()
         {
             GUILayout.BeginArea(new Rect(Screen.width - 400, 0, 400, Screen.height));
             GUILayout.Label("\n" + string.Join("\n", myLogQueue.ToArray()));
--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -8,11 +8,18 @@ namespace Com.WIAP.Checkers
     {
         private bool isConnecting = false;
 
+        /// <summary>
+        /// Called when the script starts.
+        /// </summary>
         private void Start()
         {
+            // Automatically connect to Photon when the game starts.
             ConnectToPhoton();
         }
 
+        /// <summary>
+        /// Connects to the Photon server using the configured settings.
+        /// </summary>
         public void ConnectToPhoton()
         {
             if (!PhotonNetwork.IsConnected)
@@ -22,6 +29,9 @@ namespace Com.WIAP.Checkers
             }
         }
 
+        /// <summary>
+        /// Creates a new Photon room for players to join.
+        /// </summary>
         public void CreatePhotonRoom()
         {
            if (isConnecting)
@@ -32,31 +42,78 @@ namespace Com.WIAP.Checkers
             PhotonNetwork.CreateRoom("Checkers Room", new RoomOptions { MaxPlayers = 4 });
         }
 
+        /// <summary>
+        /// Disconnects from the Photon server.
+        /// </summary>
         public void DisconnectFromPhoton()
         {
             PhotonNetwork.Disconnect();
         }
 
+        /// <summary>
+        /// Called when connected to the Photon Master Server.
+        /// </summary>
         public override void OnConnectedToMaster()
         {
             isConnecting = false;
             Debug.Log("Connected to Master");
         }
 
+        /// <summary>
+        /// Called when disconnected from the Photon server.
+        /// </summary>
+        /// <param name="cause">The reason for disconnection.</param>
         public override void OnDisconnected(DisconnectCause cause)
         {
             isConnecting = false;
             Debug.LogWarning("Disconncted from Photon: " + cause.ToString());
         }
 
+        /// <summary>
+        /// Called when a room is successfully created.
+        /// </summary>
         public override void OnCreatedRoom()
         {
             Debug.Log("Room created: " + PhotonNetwork.CurrentRoom.Name);
         }
 
+        /// <summary>
+        /// Called when creating a room fails.
+        /// </summary>
+        /// <param name="returnCode">The error code.</param>
+        /// <param name="message">The error message.</param>
         public override void OnCreateRoomFailed(short returnCode, string message)
         {
             Debug.LogError("Failed to create room: " + message);
+        }
+
+        /// <summary>
+        /// Called when a new player enters the room.
+        /// </summary>
+        /// <param name="newPlayer">The new player that entered the room.</param>
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.Log("Player entered the room");
+            LogCurrentPlayerCount();
+        }
+
+        /// <summary>
+        /// Called when a player leaves the room.
+        /// </summary>
+        /// <param name="otherPlayer">The player who left the room.</param>
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            Debug.Log("Player left the room");
+            LogCurrentPlayerCount();
+        }
+
+        /// <summary>
+        /// Logs the current player count in the room.
+        /// </summary>
+        public void LogCurrentPlayerCount()
+        {
+            if (PhotonNetwork.CurrentRoom != null)
+                Debug.Log("Current player count: " + PhotonNetwork.CurrentRoom.PlayerCount);
         }
     }
 }
